2f64459817b9ba53ce3af941962cd01c97f89a92
hypernova1 <hypemova@gmail.com>
2021-06-11 21:36:29 +0900

    init

---
+++ src/main/java/org/melchor/errander/ErranderApplication.java
@@ -0,0 +1,13 @@
+package org.melchor.errander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErranderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErranderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/melchor/errander/ErranderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.melchor.errander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErranderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
